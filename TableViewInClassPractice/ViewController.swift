9e514ac9d553b26234f790fa3af71af943b4f019
Protzman,Hannah R <s520515@ch140028m.nwmissouri.edu>
2018-02-15 12:46:48 -0600

    Initial Commit

---
+++ TableViewInClassPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewInClassPractice
+//
+//  Created by Protzman,Hannah R on 2/15/18.
+//  Copyright © 2018 Protzman,Hannah R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
